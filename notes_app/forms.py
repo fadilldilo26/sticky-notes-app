8375aafdf4f9d7954b15f5809297ff46cf6a5523
fadilldilo26 <fadill.dilo26@gmail.com>
2026-03-29 23:11:56 +0200

    Delete notes_app directory

--- notes_app/forms.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django import forms
-from .models import Note
-
-
-class NoteForm(forms.ModelForm):
-    class Meta:
-        model = Note
-        fields = ['title', 'content']
